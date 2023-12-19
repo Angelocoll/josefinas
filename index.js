0bfb36d58ee4133c22abfed8621599d42e772bee
Angelo <143688799+Angelocoll@users.noreply.github.com>
2023-12-20 00:25:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// Ange det specifika datumet och tiden att räkna ned från
-const targetDate = new Date("December 31, 2023 18:00:00").getTime(); // Ange ditt önskade datum här
+const targetDate = new Date("December 31, 2023 18:00:00").getTime();
 
 let playBtn = document.querySelector("#play");
 playBtn.addEventListener("click", () => {
   let audio = document.querySelector("#musik");
   audio.play();
   const countdown = setInterval(() => {
-    const now = new Date().getTime(); // Nuvarande tid
-    const distance = targetDate - now; // Beräkna skillnaden mellan måldatum och nuvarande tid
+    const now = new Date().getTime();
+    const distance = targetDate - now;
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = formatTime(
@@ -18,20 +17,18 @@ playBtn.addEventListener("click", () => {
     );
     const seconds = formatTime(Math.floor((distance % (1000 * 60)) / 1000));
 
-    // Uppdatera element på sidan med den återstående tiden
     document.querySelector(".D").innerHTML = days;
     document.querySelector(".H").innerHTML = hours;
     document.querySelector(".M").innerHTML = minutes;
     document.querySelector(".S").innerHTML = seconds;
 
-    // Om nedräkningen är klar
     if (distance < 0) {
-      clearInterval(countdown); // Stoppa nedräkningen
-      // Här kan du lägga till kod för att visa ett meddelande när nedräkningen är klar
+      clearInterval(countdown);
+
       document.querySelector(".clock").innerHTML =
         "Årets sista och största fest har startat!";
     }
-  }, 1000); // Uppdatera varje sekund
+  }, 1000);
 
   function formatTime(time) {
     return time < 10 ? "0" + time : time;
@@ -41,6 +38,7 @@ playBtn.addEventListener("click", () => {
 
     <video autoplay muted playsinline loop src="pexels_videos_1741279 (1080p).mp4" alt="raketer">
 </div>
+<h2>NyårsFest</h2>
   <div class="clock">
   <h1>
      <span class="D">00</span> : <span class="H">00</span> : <span class="M">00</span> : <span class="S">00</span>
@@ -54,3 +52,17 @@ playBtn.addEventListener("click", () => {
 </div>
 <audio id="musik" autoplay loop playsinline src="passion-127011.mp3"></audio>`;
 });
+function transformText() {
+  setInterval(function () {
+    let currentText = document.querySelector("h2").innerText;
+
+    if (currentText === "NyårsFest") {
+      document.querySelector("h2").innerText = "Josefina Djurgården";
+    } else if (currentText === "Josefina Djurgården") {
+      document.querySelector("h2").innerText = "31 Dec 18.00 - 05.00";
+    } else {
+      document.querySelector("h2").innerText = "NyårsFest";
+    }
+  }, 2500);
+}
+transformText();
